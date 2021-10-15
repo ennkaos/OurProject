8c49944c16c669b70bd5ce2efbf84a4d5ef9ad1e
Alexandru Florin Bucur <alexandru.f.bucur@avanade.com>
2021-10-15 17:46:12 +0200

    Third Commit

---
+++ src/app/tracker/tracker/tracker.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TrackerComponent } from '../tracker.component';
+import { ContactComponent } from 'src/app/contact/contact.component';
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: 'tracker', component: TrackerComponent },
+      { path: 'tracker/conta', component: TrackerComponent },
+    ]),
+  ],
+})
+export class TrackerModule {}
