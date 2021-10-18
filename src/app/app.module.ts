3981910187fb7ff249b663ec23e300d733c6e27a
Giulia-M <giuliamaggio16@gmail.com>
2021-10-18 17:12:46 +0200

    giulia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,28 @@ import { TrackerComponent } from './tracker/tracker.component';
 import { ContactComponent } from './contact/contact.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { TracherModule } from './tracker/tracher/tracher.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrackerComponent,
     ContactComponent,
     HeaderComponent,
     FooterComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot([
+    {
+      path: 'home',
+      component: MainComponent
+    },
+    {
+      path: 'tracker',
+      loadChildren: () => import('./tracker/tracher/tracher.module').then(m => m.TracherModule)
+    }
+
+  ])],
+
   providers: [],
   bootstrap: [AppComponent],
 })
